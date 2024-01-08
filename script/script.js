059a65c4923626abb3e8fcab4231fb40d4a4d070
hazeroj <jhy970811@gmail.com>
2024-01-08 15:56:47 +0900

    투쿨포스쿨 메인페이지

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$('document').ready(function(){
+// 변수선언 
+
+
+// 내비게이션 등장
+// $('.fa-bars').mouseenter(function(){
+//     $('.gnb_wrap').css({'left':'0px'})
+// })
+
+// $('.fa-bars').mouseleave(function(){
+//     $('.gnb_wrap').css({'left':'-1000px'})
+}
+
+)
+
+
+
+})
